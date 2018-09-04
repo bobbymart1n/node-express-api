137368bc0ed31dc82c7fa8c047d2231da76c7875
Bobby Martin <robert.martin.619@gmail.com>
2018-09-03 21:32:27 -0700

    Add new working put method

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -26,6 +26,19 @@ module.exports = function(app, db) {
     })
   })
 
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if(err) {
+        res.send({ 'error': 'An error has occured' });
+      } else {
+        res.send(note);
+      }
+    })
+  })
+
   const collection =
     app.post('/notes', (req, res) => {
       const note = { text: req.body.body, title: req.body.title };
